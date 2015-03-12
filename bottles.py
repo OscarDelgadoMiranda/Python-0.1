f1c6ccd5721754c508e1484cc64b42a85fa4dab4
OscarDelgadoMiranda <OscarDelgadoMiranda@users.noreply.github.com>
2015-03-12 13:30:17 +0100

    Seminario 1 - Ejercicio 1

--- bottles.py
+++ bottles.py
@@ -3,6 +3,6 @@ def MySong(bottles):
 	for i in range(bottles):
 		print bottles-i, "bottles of beer on the wall,", bottles-i, "bottles of beer."
 		print "Take one down, pass it around,", bottles-i-1, "bottles of beer on the wall."
-		
+	
 #Llamada a la funcion con el numero de bolletas que quieras
 MySong(99)
